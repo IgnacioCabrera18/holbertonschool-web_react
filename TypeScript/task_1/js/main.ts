001b2169980b0bc29b679a7183bdf8ce7349362e
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-01-28 14:18:43 +0000

    Done

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -40,3 +40,34 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
 };
 
 console.log(printTeacher("John", "Doe"));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student: StudentClassInterface = new StudentClass("Jane", "Smith");
+console.log(student.displayName());
+console.log(student.workOnHomework());
