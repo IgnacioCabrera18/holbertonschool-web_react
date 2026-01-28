bc175b99f5507daf2b676a3fc4ca85ad62b7964e
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-01-28 14:50:32 +0000

    Done

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,13 +1,13 @@
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreack(): string;
-  workDirectorTask(): string;
+  workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreack(): string;
-  workTeacherTask(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -17,7 +17,7 @@ class Director implements DirectorInterface {
   getCoffeeBreack(): string {
     return 'Getting a coffee break';
   }
-  workDirectorTask(): string {
+  workDirectorTasks(): string {
     return 'Getting to director tasks';
   }
 }
@@ -29,7 +29,7 @@ class Teacher implements TeacherInterface {
   getCoffeeBreack(): string {
     return 'Cannot have a break';
   }
-  workTeacherTask(): string {
+  workTeacherTasks(): string {
     return 'Getting to work';
   }
 }
@@ -44,3 +44,18 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
